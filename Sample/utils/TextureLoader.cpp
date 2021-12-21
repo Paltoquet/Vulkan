556deb0ff4fc65d752b3e52eb0fe40f0032ba7bc
Paltoquet <thibault.ober@gmail.com>
2021-12-21 21:26:47 +0100

    refactor texture loading

--- Sample/utils/TextureLoader.cpp
+++ Sample/utils/TextureLoader.cpp
@@ -50,15 +50,14 @@ ImageView TextureLoader::loadTexture(const std::string& path, const VkFormat& fo
         VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
         imageInfo.Vkimage, imageInfo.Vkmemory);
 
-    transitionImageLayout(imageInfo, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-    copyBufferToImage(stagingBuffer, imageInfo.Vkimage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
-    //transitioned to VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL while generating mipmaps
-    //transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, m_mipLevels);
+    VkCommandBuffer transitionCmd = m_renderContext->beginSingleTimeCommands();
+    setImageLayout(transitionCmd, imageInfo, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+    copyBufferToImage(transitionCmd, stagingBuffer, imageInfo.Vkimage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
+    generateMipmaps(transitionCmd, imageInfo, texWidth, texHeight);
+    m_renderContext->endSingleTimeCommands(transitionCmd);
 
     vkDestroyBuffer(m_renderContext->device(), stagingBuffer, nullptr);
     vkFreeMemory(m_renderContext->device(), stagingBufferMemory, nullptr);
-
-    generateMipmaps(imageInfo, texWidth, texHeight);
 
     return ImageView(m_renderContext->device(), imageInfo, VK_IMAGE_VIEW_TYPE_2D);
 }
@@ -104,165 +103,23 @@ ImageView TextureLoader::loadNoiseTexture(const VkExtent2D& dimension, const VkF
         VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
         imageInfo.Vkimage, imageInfo.Vkmemory);
 
-    transitionImageLayout(imageInfo, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-    copyBufferToImage(stagingBuffer, imageInfo.Vkimage, static_cast<uint32_t>(dimension.width), static_cast<uint32_t>(dimension.height));
-    //transitioned to VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL while generating mipmaps
-    transitionImageLayout(imageInfo, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+    VkCommandBuffer copyCmd = m_renderContext->beginSingleTimeCommands();
 
+    setImageLayout(copyCmd, imageInfo, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+    copyBufferToImage(copyCmd, stagingBuffer, imageInfo.Vkimage, static_cast<uint32_t>(dimension.width), static_cast<uint32_t>(dimension.height));
+    setImageLayout(copyCmd, imageInfo, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+    m_renderContext->endSingleTimeCommands(copyCmd);
 
     vkDestroyBuffer(m_renderContext->device(), stagingBuffer, nullptr);
     vkFreeMemory(m_renderContext->device(), stagingBufferMemory, nullptr);
-
     //generateMipmaps(imageInfo, dimension.width, dimension.height);
 
     return ImageView(m_renderContext->device(), imageInfo, VK_IMAGE_VIEW_TYPE_2D);
 }
 
-ImageView TextureLoader::loadNoiseTexture3D(const VkExtent3D& dimension, VkImageAspectFlags aspect)
-{
-    Image img;
-    img.mipLevels = 1;
-    img.Vkformat = VK_FORMAT_R8_UNORM;
-    img.aspectFlag = aspect;
-
-    /* ----------------------- Loading datas ----------------------- */
-    const uint32_t texMemSize = dimension.width * dimension.height * dimension.depth;
-    uint8_t *data = new uint8_t[texMemSize];
-    memset(data, 0, texMemSize);
-
-    BrownianNoise3D noiseGenerator = BrownianNoise3D(glm::ivec3(64, 64, 64), 3);
-    uint8_t *noiseDatas = new uint8_t[texMemSize];
-    size_t pageSize = dimension.width * dimension.height;
-
-    glm::vec3 pixelPos;
-    float noiseValue;
-    unsigned char value;
-    size_t index;
-    for (size_t k = 0; k < dimension.depth; k++) {
-        for (size_t j = 0; j < dimension.height; j++) {
-            for (size_t i = 0; i < dimension.width; i++) {
-                pixelPos = glm::vec3(i, j, k);
-                index = k * pageSize + j * dimension.width + i;
-                //noiseValue = noiseGenerator.evaluate(pixelPos) * 255.0f;
-                noiseValue = 1.0; // k / float(dimension.depth);
-                noiseValue = 64;
-                value = static_cast<uint8_t>(noiseValue);
-                noiseDatas[index] = value;
-            }
-        }
-        //std::cout << "noise val: " << noiseDatas[index] << std::endl;
-    }
-
-    VkBuffer stagingBuffer;
-    VkDeviceMemory stagingBufferMemory;
-    m_renderContext->createBuffer(texMemSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
-    uint8_t* bufferData;
-    vkMapMemory(m_renderContext->device(), stagingBufferMemory, 0, texMemSize, 0, (void **)&bufferData);
-    memcpy(data, noiseDatas, texMemSize);
-    vkUnmapMemory(m_renderContext->device(), stagingBufferMemory);
-
-    //
-    //vk_initializer::createImage(m_renderContext->device(), m_renderContext->physicalDevice(), dimension.width, dimension.height, 
-    //imageInfo.mipLevels, VK_SAMPLE_COUNT_1_BIT, imageInfo.Vkformat, VK_IMAGE_TILING_OPTIMAL,
-    //VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-    //    imageInfo.Vkimage, imageInfo.Vkmemory);
-
-    //ImageCreateInfo imageInfo{};
-    //ageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
-    //ageInfo.imageType = VK_IMAGE_TYPE_2D;
-    //ageInfo.extent.width = width;
-    //ageInfo.extent.height = height;
-    //ageInfo.extent.depth = 1;
-    //ageInfo.mipLevels = mip_levels;
-    //ageInfo.arrayLayers = 1;
-    //ageInfo.format = format;
-    //ageInfo.tiling = tiling;
-    //ageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    //ageInfo.usage = usage;
-    //ageInfo.samples = nbSamples;
-    //ageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-    //
-    // (vkCreateImage(device, &imageInfo, nullptr, &image) != VK_SUCCESS) {
-    //  throw std::runtime_error("failed to create image!");
-    //
-    //
-    //MemoryRequirements memRequirements;
-    //GetImageMemoryRequirements(device, image, &memRequirements);
-    //
-    //MemoryAllocateInfo allocInfo{};
-    //locInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-    //locInfo.allocationSize = memRequirements.size;
-    //locInfo.memoryTypeIndex = findMemoryType(physicalDevice, memRequirements.memoryTypeBits, properties);
-    //
-    // (vkAllocateMemory(device, &allocInfo, nullptr, &imageMemory) != VK_SUCCESS) {
-    //  throw std::runtime_error("failed to allocate image memory!");
-    //
-    //
-    //BindImageMemory(device, image, imageMemory, 0);
-
-
-    // ----------------------- ImageInfo -----------------------
-    VkImageCreateInfo imageCreateInfo{};
-    imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
-    imageCreateInfo.imageType = VK_IMAGE_TYPE_3D;
-    imageCreateInfo.extent.width = dimension.width;
-    imageCreateInfo.extent.height = dimension.height;
-    imageCreateInfo.extent.depth = dimension.depth;
-    imageCreateInfo.mipLevels = img.mipLevels;
-    imageCreateInfo.arrayLayers = 1;
-    imageCreateInfo.format = img.Vkformat;
-    imageCreateInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
-    imageCreateInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    imageCreateInfo.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
-    imageCreateInfo.samples = VK_SAMPLE_COUNT_1_BIT;
-    imageCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-
-    if (vkCreateImage(m_renderContext->device(), &imageCreateInfo, nullptr, &img.Vkimage) != VK_SUCCESS) {
-        throw std::runtime_error("failed to create image!");
-    }
-
-    VkMemoryRequirements memRequirements;
-    vkGetImageMemoryRequirements(m_renderContext->device(), img.Vkimage, &memRequirements);
-
-    VkMemoryAllocateInfo allocInfo{};
-    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-    allocInfo.allocationSize = memRequirements.size;
-    allocInfo.memoryTypeIndex = vk_initializer::findMemoryType(m_renderContext->physicalDevice(), memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-
-    vkAllocateMemory(m_renderContext->device(), &allocInfo, nullptr, &img.Vkmemory);
-    vkBindImageMemory(m_renderContext->device(), img.Vkimage, img.Vkmemory, 0);
-
-    transitionImageLayout(img, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-    copyBufferToImage(stagingBuffer, img.Vkimage, static_cast<uint32_t>(dimension.width), static_cast<uint32_t>(dimension.height), static_cast<uint32_t>(dimension.depth));
-    transitionImageLayout(img, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-
-    // ----------------------- ImageView -----------------------
-    /*VkImageViewCreateInfo viewInfo{};
-    viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-    viewInfo.image = img.Vkimage;
-    viewInfo.viewType = VK_IMAGE_VIEW_TYPE_3D;
-    viewInfo.format = img.Vkformat;
-    viewInfo.components = { VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_A };
-    viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-    viewInfo.subresourceRange.baseMipLevel = 0;
-    viewInfo.subresourceRange.baseArrayLayer = 0;
-    viewInfo.subresourceRange.layerCount = 1;
-    viewInfo.subresourceRange.levelCount = 1;*/
-
-    /*VkImageView view;
-    if (vkCreateImageView(m_renderContext->device(), &viewInfo, nullptr, &view) != VK_SUCCESS) {
-        throw std::runtime_error("failed to create texture image view!");
-    }*/
-
-    vkDestroyBuffer(m_renderContext->device(), stagingBuffer, nullptr);
-    vkFreeMemory(m_renderContext->device(), stagingBufferMemory, nullptr);
-
-    return ImageView(m_renderContext->device(), img, VK_IMAGE_VIEW_TYPE_3D);
-}
-
 /* --------------------------------- Private methods --------------------------------- */
 
-void TextureLoader::generateMipmaps(const Image& image, int32_t texWidth, int32_t texHeight) {
+void TextureLoader::generateMipmaps(VkCommandBuffer commandBuffer, Image& image, int32_t texWidth, int32_t texHeight) {
 
     // Check if image format supports linear blitting
     VkFormatProperties formatProperties;
@@ -271,8 +128,6 @@ void TextureLoader::generateMipmaps(const Image& image, int32_t texWidth, int32_
     if (!(formatProperties.optimalTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT)) {
         throw std::runtime_error("texture image format does not support linear blitting!");
     }
-
-    VkCommandBuffer commandBuffer = m_renderContext->beginSingleTimeCommands();
 
     VkImageMemoryBarrier barrier{};
     barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
@@ -346,80 +201,136 @@ void TextureLoader::generateMipmaps(const Image& image, int32_t texWidth, int32_
         0, nullptr,
         0, nullptr,
         1, &barrier);
-
-    m_renderContext->endSingleTimeCommands(commandBuffer);
 }
 
-void TextureLoader::transitionImageLayout(const Image& image, VkImageLayout oldLayout, VkImageLayout newLayout) {
-    VkCommandBuffer commandBuffer = m_renderContext->beginSingleTimeCommands();
-    VkImageMemoryBarrier barrier{};
-    barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-    barrier.oldLayout = oldLayout;
-    barrier.newLayout = newLayout;
-    barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-    barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-    barrier.image = image.Vkimage;
-    barrier.subresourceRange.baseMipLevel = 0;
-    barrier.subresourceRange.levelCount = image.mipLevels;
-    barrier.subresourceRange.baseArrayLayer = 0;
-    barrier.subresourceRange.layerCount = 1;
+void TextureLoader::setImageLayout(VkCommandBuffer commandBuffer, Image& image, VkImageLayout oldImageLayout, VkImageLayout newImageLayout,
+    VkPipelineStageFlags srcStageMask, VkPipelineStageFlags dstStageMask)
+{
+    // The sub resource range describes the regions of the image we will be transitioned
+    VkImageSubresourceRange subresourceRange = {};
+    subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    subresourceRange.baseMipLevel = 0;
+    subresourceRange.levelCount = image.mipLevels;
+    subresourceRange.layerCount = 1;
 
-    /* Color, Depth and Stencil*/
-    if (newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
-        barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+    // Create an image barrier object
+    VkImageMemoryBarrier imageMemoryBarrier{};
+    imageMemoryBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+    imageMemoryBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    imageMemoryBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    imageMemoryBarrier.oldLayout = oldImageLayout;
+    imageMemoryBarrier.newLayout = newImageLayout;
+    imageMemoryBarrier.image = image.Vkimage;
+    imageMemoryBarrier.subresourceRange = subresourceRange;
 
-        if (hasStencilComponent(image.Vkformat)) {
-            barrier.subresourceRange.aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
+    // Source layouts (old)
+    // Source access mask controls actions that have to be finished on the old layout
+    // before it will be transitioned to the new layout
+    switch (oldImageLayout)
+    {
+    case VK_IMAGE_LAYOUT_UNDEFINED:
+        // Image layout is undefined (or does not matter)
+        // Only valid as initial layout
+        // No flags required, listed only for completeness
+        imageMemoryBarrier.srcAccessMask = 0;
+        break;
+
+    case VK_IMAGE_LAYOUT_PREINITIALIZED:
+        // Image is preinitialized
+        // Only valid as initial layout for linear images, preserves memory contents
+        // Make sure host writes have been finished
+        imageMemoryBarrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
+        // Image is a color attachment
+        // Make sure any writes to the color buffer have been finished
+        imageMemoryBarrier.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
+        // Image is a depth/stencil attachment
+        // Make sure any writes to the depth/stencil buffer have been finished
+        imageMemoryBarrier.srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL:
+        // Image is a transfer source
+        // Make sure any reads from the image have been finished
+        imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
+        // Image is a transfer destination
+        // Make sure any writes to the image have been finished
+        imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
+        // Image is read by a shader
+        // Make sure any shader reads from the image have been finished
+        imageMemoryBarrier.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+        break;
+    default:
+        // Other source layouts aren't handled (yet)
+        break;
+    }
+
+    // Target layouts (new)
+    // Destination access mask controls the dependency for the new image layout
+    switch (newImageLayout)
+    {
+    case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
+        // Image will be used as a transfer destination
+        // Make sure any writes to the image have been finished
+        imageMemoryBarrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL:
+        // Image will be used as a transfer source
+        // Make sure any reads from the image have been finished
+        imageMemoryBarrier.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
+        // Image will be used as a color attachment
+        // Make sure any writes to the color buffer have been finished
+        imageMemoryBarrier.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
+        // Image layout will be used as a depth/stencil attachment
+        // Make sure any writes to depth/stencil buffer have been finished
+        imageMemoryBarrier.dstAccessMask = imageMemoryBarrier.dstAccessMask | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+        break;
+
+    case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
+        // Image will be read in a shader (sampler, input attachment)
+        // Make sure any writes to the image have been finished
+        if (imageMemoryBarrier.srcAccessMask == 0)
+        {
+            imageMemoryBarrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT | VK_ACCESS_TRANSFER_WRITE_BIT;
         }
-    }
-    else {
-        barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-    }
-
-    VkPipelineStageFlags sourceStage;
-    VkPipelineStageFlags destinationStage;
-
-    if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
-        barrier.srcAccessMask = 0;
-        barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-
-        sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-        destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
-    }
-    else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
-        barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-        barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-
-        sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
-        destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
-    }
-    else if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
-        barrier.srcAccessMask = 0;
-        barrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-
-        sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-        destinationStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
-    }
-    else {
-        throw std::invalid_argument("unsupported layout transition!");
+        imageMemoryBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+        break;
+    default:
+        // Other source layouts aren't handled (yet)
+        break;
     }
 
-
+    // Put barrier inside setup command buffer
     vkCmdPipelineBarrier(
         commandBuffer,
-        sourceStage, destinationStage,
+        srcStageMask,
+        dstStageMask,
         0,
         0, nullptr,
         0, nullptr,
-        1, &barrier
-    );
-    m_renderContext->endSingleTimeCommands(commandBuffer);
+        1, &imageMemoryBarrier);
 }
 
-
-void TextureLoader::copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height, uint32_t depth) {
-    VkCommandBuffer commandBuffer = m_renderContext->beginSingleTimeCommands();
-
+void TextureLoader::copyBufferToImage(VkCommandBuffer commandBuffer, VkBuffer buffer, VkImage image, uint32_t width, uint32_t height, uint32_t depth) 
+{
     VkBufferImageCopy region{};
     region.bufferOffset = 0;
     region.bufferRowLength = 0;
@@ -445,8 +356,6 @@ void TextureLoader::copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t w
         1,
         &region
     );
-
-    m_renderContext->endSingleTimeCommands(commandBuffer);
 }
 
 bool TextureLoader::hasStencilComponent(VkFormat format) {
--- Sample/utils/TextureLoader.h
+++ Sample/utils/TextureLoader.h
@@ -12,15 +12,14 @@ public:
 public:
     ImageView loadTexture(const std::string& path, const VkFormat& format, VkImageAspectFlags aspect);
     ImageView loadNoiseTexture(const VkExtent2D& dimension, const VkFormat& format, VkImageAspectFlags aspect);
-    ImageView loadNoiseTexture3D(const VkExtent3D& dimension, VkImageAspectFlags aspect);
-    /*ImageView loadCubeMap(const std::string& front, const std::string& back, const std::string& up,
-                          const std::string& down, const std::string& left, const std::string& right,
-                          const VkFormat& format, VkImageAspectFlags aspect)*/
 
 private:
-    void generateMipmaps(const Image& image, int32_t texWidth, int32_t texHeight);
-    void transitionImageLayout(const Image& image, VkImageLayout oldLayout, VkImageLayout newLayout);
-    void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height, uint32_t depth = 1);
+    void generateMipmaps(VkCommandBuffer commandBuffer, Image& image, int32_t texWidth, int32_t texHeight);
+    void setImageLayout(VkCommandBuffer commandBuffer, Image& image, VkImageLayout oldImageLayout, VkImageLayout newImageLayout,
+        VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+        VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+        // Create an image barrier object
+    void copyBufferToImage(VkCommandBuffer commandBuffer, VkBuffer buffer, VkImage image, uint32_t width, uint32_t height, uint32_t depth = 1);
     bool hasStencilComponent(VkFormat format);
 
 private:
